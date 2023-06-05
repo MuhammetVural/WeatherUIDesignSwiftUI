3b1239714f8e9be2b9c6e5ea3a778ec33515ee36
Muhammet <muhammetvural@anadolu.edu.tr>
2023-06-05 15:43:14 +0300

    Initial Commit

---
+++ WeatherUIDesignSwiftUI/WeatherUIDesignSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUIDesignSwiftUIApp.swift
+//  WeatherUIDesignSwiftUI
+//
+//  Created by Muhammet Vural on 5.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUIDesignSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
